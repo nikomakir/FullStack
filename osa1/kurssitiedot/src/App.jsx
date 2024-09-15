ec5adba9e546f34c3da9a97bf4bbcf839cdff778
Niko Raulas <niko.raulas@outlook.com>
2024-09-15 17:07:23 +0300

    osa1 tehtävä kurssitiedot

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,63 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.course.parts[0].name} excercises={props.course.parts[0].exercises} />
+      <Part name={props.course.parts[1].name} excercises={props.course.parts[1].exercises} />
+      <Part name={props.course.parts[2].name} excercises={props.course.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.excercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of excercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name:'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name:'State of a component',
+      exercises: 14
+    }
+  ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default App
