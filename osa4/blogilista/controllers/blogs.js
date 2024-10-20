7622d0d99b8c2005ccf31f1f7541dfb25f5fdec3
Niko Raulas <niko.raulas@outlook.com>
2024-10-20 23:13:44 +0300

    tehtävä 4.17

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,9 +1,11 @@
 const blogsRouter = require('express').Router()
-const { response } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
@@ -12,15 +14,21 @@ blogsRouter.post('/', async (request, response) => {
   if (!body.title | !body.url) {
     response.status(400).end()
   }
+  const allUsers = await User.find({})
+  const user = allUsers[0]
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
