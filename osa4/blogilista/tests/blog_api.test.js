e7c3964d6c0c334b818005c642855f3665a1414b
Niko Raulas <niko.raulas@outlook.com>
2024-10-20 21:08:38 +0300

    tehtävät 4.15 ja 4.16

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -6,7 +6,6 @@ const app = require('../app')
 const api = supertest(app)
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
-const blog = require('../models/blog')
 
 
 beforeEach(async () => {
