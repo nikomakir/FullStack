5d188a0a3342d875ddeefd32b7553cfac88da6e7
Niko Raulas <niko.raulas@outlook.com>
2024-11-16 18:58:38 +0200

    korjattu välilyönnit

--- osa6/redux-anecdotes/src/components/Filter.jsx
+++ osa6/redux-anecdotes/src/components/Filter.jsx
@@ -3,22 +3,22 @@ import { useDispatch } from 'react-redux'
 
 
 const Filter = () => {
-	const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-	const handleChange = (event) => {
-		const filter = event.target.value
-		dispatch(filterChange(filter))
-	}
+  const handleChange = (event) => {
+    const filter = event.target.value
+    dispatch(filterChange(filter))
+  }
 
-	const style = {
-		marginBottom: 10
-	}
+  const style = {
+    marginBottom: 10
+  }
 
-	return (
-		<div style={style}>
-			filter <input onChange={handleChange} />
-		</div>
-	)
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
 }
 
 export default Filter
